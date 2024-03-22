24f6c3893b3a3fbe80be07568dcd5509cbd9579e
Dariusz Ptak <dariusz.ptak@op.pl>
2024-03-22 12:33:11 +0100

    Initial commit

--- src/test/java/config/MotoConfig.java
+++ src/test/java/config/MotoConfig.java
@@ -5,7 +5,6 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
-import io.restassured.specification.ResponseSpecification;
 import org.junit.BeforeClass;
 
 public class MotoConfig {
